b3ad0207a1b56f923b20199b9576890d02640b33
Callum <callumdav4312@gmail.com>
2026-01-04 14:43:08 +0000

    skelelton landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,60 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center p-10">
-      <h1 className="text-5xl font-bold">New Dog, Old Tricks</h1>
-      <p className="mt-6 text-xl text-gray-400 text-center max-w-xl">
-        Classic sales principles. Modern execution.
-        A real product, not a course.
-      </p>
-    </main>
+    <div>
+      <header>
+        <div>New Dog, Old Tricks</div>
+        <nav>
+          <a href="#masterclass">Masterclasss</a>{" "}
+          <a href="#how">How it works</a>{" "}
+          <a href="#about">About</a>
+        </nav>
+      </header>
+
+      <main>
+        <section>
+          <h1>Old school persuasion. Modern execution.</h1>
+          <p>
+            An interactive sales learning site: short lessons, from real people, with real experience
+          </p>
+          <button>Explore curriculum</button>
+        </section>
+
+        <section id="The Masterclass">
+          <h2>Lessons</h2>
+          <p>Different salesman. Their stories - built as short practical modules.</p>
+
+          <div>
+            <article>
+              <h3>Alan</h3>
+              <p>Salesman for blah blah blah</p>
+            </article>
+
+            <article>
+              <h3>Richard</h3>
+              <p>Owner of mulitple hihg end restaurant-hotels</p>
+            </article>
+
+            <article>
+              <h3>Execution</h3>
+              <p>Scripts, follow-ups, and feedback loops you can measure.</p>
+            </article>
+          </div>
+        </section>
+
+        <section id="how">
+          <h2>How it works</h2>
+          <ol>
+            <li>Just start the masterclass and stop anytime you'd like</li>
+            <li>You can skip to lessons you'd like to</li>
+          </ol>
+        </section>
+      </main>
+
+      <footer id="about">
+        <p>
+          Built as a personal project. The content from these lessons are from my own research, interviews and extrapolation. 
+        </p>
+      </footer>
+    </div>
   );
 }
